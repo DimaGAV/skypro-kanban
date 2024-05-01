fc31e3fa1ceb3d419cc69447166c7020b807a606
DimaGAV <gav_gav82@mail.ru>
2024-05-01 15:36:58 +0300

    22

--- src/styled/common.js
+++ src/styled/common.js
@@ -1,14 +1,6 @@
 import styled from "styled-components";
 import { breakpoints } from "../lib/breakpoints";
 
-export const Wrapper = styled.div`
-max-width: 100%;
-  width: 100vw;
-  min-height: 100vh;
-  overflow: hidden;
-  background-color: #F1F1F1;
-`;
-
 export const Container = styled.div`
   max-width: 1260px;
   width: 100%;
@@ -19,4 +11,12 @@ export const Container = styled.div`
     width: 100%;
     padding: 0 16px;
   }
+`;
+
+export const Wrapper = styled.div`
+max-width: 100%;
+  width: 100vw;
+  min-height: 100vh;
+  overflow: hidden;
+  background-color: #F1F1F1;
 `;
