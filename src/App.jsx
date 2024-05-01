77e42d9a47056d42107f1b19c04d7a64bfb188b6
DimaGAV <gav_gav82@mail.ru>
2024-05-01 18:47:05 +0300

    вход и регистрация верстка1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Login from "./pages/Login/Login";
 import Exit from "./pages/Exit/Exit";
 import NotFound from "./pages/NotFound/NotFound";
 import Register from "./pages/Register/Register";
+import { useState } from "react";
 
 export const AppRoutes = {
   MAIN: "/",
@@ -16,9 +17,9 @@ export const AppRoutes = {
   REGISTER: "/register",
 };
 
+export let isAuth;
 export default function App() {
-  const isAuth = false;
-
+  
   return (
     <Routes>
       <Route element={<PrivateRoute isAuth={isAuth} />}>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,26 +1,28 @@
 import { Link } from "react-router-dom";
 import { AppRoutes } from "../../App";
+import * as S from "../../components/Form/Form";
+// import { isAuth } from "../../App";
 
 export default function Login() {
   return (
-    <form>
-      <h1>Вход</h1>
-      {/* Если пользователь уже авторизован, 
+    <S.Form>
+      <S.FormContainer>
+        <S.FormHeader>Вход</S.FormHeader>
+        {/* Если пользователь уже авторизован, 
       то будет переход на главную. 
       
       В противном случае переход не произойдет */}
-      <div>
-        <input type="mail" />
-        <input type="password" />
-        <div></div>
+        <S.FormInput type="mail" placeholder="Эл. почта" />
+        <S.FormInput type="password" placeholder="Пароль" />
+
         <Link to={AppRoutes.MAIN}>
-          <button>Войти</button>
+          <S.FormButton /* onClick = {} */>Войти</S.FormButton>
         </Link>
-      </div>
-      <div>
-        <p>Нужно зарегестрироваться?</p>
-        <Link to={AppRoutes.REGISTER}>Регистрируйтесь здесь</Link>
-      </div>
-    </form>
+        <S.FormFooter>
+          <p>Нужно зарегестрироваться?</p>
+          <Link to={AppRoutes.REGISTER}>Регистрируйтесь здесь</Link>
+        </S.FormFooter>
+      </S.FormContainer>
+    </S.Form>
   );
 }
